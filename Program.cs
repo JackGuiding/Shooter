f841e76e4fefd0edaa6a53ab18a53e001bf5b46b
Jackutea <chenwansal1@163.com>
2024-03-22 21:01:51 +0800

    ESTABLISH

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+
+public static class Program
+{
+
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 600, "Hello World");
+
+        while(!Raylib.WindowShouldClose())
+        {
+            // 开始绘制
+            Raylib.BeginDrawing();
+
+            // 画背景
+            Raylib.ClearBackground(Color.RayWhite);
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            
+            // 结束绘制
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+
+    }
+
+}
